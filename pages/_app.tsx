e0c2923080d3e0cc373c7d3aa2e1f9be3b3edc11
Nam JangHyun <smash009@naver.com>
2022-12-30 18:00:33 +0900

    refactor: css style jsx global

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import { AppProps } from "next/app";
 import NavBar from "../components/NavBar";
-// import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
