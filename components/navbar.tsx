7b5d9654302e6c879f1eed8b00230742212f0066
Nam JangHyun <smash009@naver.com>
2022-12-30 12:51:01 +0900

    Feat:

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,24 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function NavBar() {
+  const router = useRouter();
+  console.log(router);
+
   return (
     <nav>
-      <Link href="/" className="firstLink" style={{ color: "green" }}>
+      <Link
+        href="/"
+        className="firstLink"
+        style={{ color: router.pathname === "/" ? "red" : "black" }}
+      >
         Home
       </Link>
-      <Link href="/about" className="secondLink" style={{ color: "green" }}>
+      <Link
+        href="/about"
+        className="secondLink"
+        style={{ color: router.pathname === "/about" ? "red" : "black" }}
+      >
         About
       </Link>
     </nav>
