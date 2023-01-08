cfbcc8d5d1eccf2250d0a9e6f2f56f84c09aaa11
Nam JangHyun <smash009@naver.com>
2023-01-09 02:19:55 +0900

    Feat: 이미지, 제목 출력

--- next.config.js
+++ next.config.js
@@ -11,6 +11,9 @@ const nextConfig = {
       },
     ];
   },
+  images: {
+    domains: ["image.tmdb.org"], // 이곳에 에러에서 hostname 다음 따옴표에 오는 링크를 적으면 됨
+  },
 };
 
 module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,59 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
+import Image from "next/image";
 
 export default function Home() {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     (async () => {
       const response = await fetch(`/api/movies`);
-      const json = await response.json();
-
-      console.log(json);
+      const result = await response.json();
+      console.log(result.results);
+      setMovies(result.results);
     })();
   }, []);
 
   return (
-    <div>
-      <Seo title="Home" />
-      <h1>Hello</h1>
-    </div>
+    <>
+      <div>
+        <Seo title="Home" />
+        {!movies && <h4>Loading...</h4>}
+        {movies?.map((movie: any) => {
+          return (
+            <div className="movie" key={movie.id}>
+              <Image
+                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                alt={`${movie.original_title}`}
+                width={300}
+                height={450}
+              />
+              <h4>{movie.original_title}</h4>
+            </div>
+          );
+        })}
+      </div>
+
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
+    </>
   );
 }
