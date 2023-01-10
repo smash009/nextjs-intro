5831300dc8b7c2163a086df407d423defee203e0
Nam JangHyun <smash009@naver.com>
2023-01-10 10:29:32 +0900

    Feat: getServerSideProps 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,30 +2,20 @@ import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 import Image from "next/image";
 
-export default function Home() {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const response = await fetch(`/api/movies`);
-      const result = await response.json();
-      console.log(result.results);
-      setMovies(result.results);
-    })();
-  }, []);
-
+export default function Home({ results }: any) {
   return (
     <>
-      <div>
+      <div className="container">
         <Seo title="Home" />
-        {!movies && <h4>Loading...</h4>}
-        {movies?.map((movie: any) => {
+        {results?.map((movie: any) => {
           return (
             <div className="movie" key={movie.id}>
               <Image
                 src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
                 alt={`${movie.original_title}`}
-                width={300}
-                height={450}
+                width={220}
+                height={270}
+                className="movie_poster"
               />
               <h4>{movie.original_title}</h4>
             </div>
@@ -40,20 +30,42 @@ export default function Home() {
           padding: 20px;
           gap: 20px;
         }
-        .movie img {
+
+        .movie {
+          cursor: pointer;
+        }
+
+         {
+          /* .movie_poster {
           max-width: 100%;
           border-radius: 12px;
           transition: transform 0.2s ease-in-out;
           box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
         }
-        .movie:hover img {
+        .movie:hover .movie_poster {
           transform: scale(1.05) translateY(-10px);
+        } */
         }
+
         .movie h4 {
+          width: 200px;
+          margin: 0 auto;
+          padding: 5px;
           font-size: 18px;
           text-align: center;
         }
       `}</style>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const response = await fetch(`http://localhost:3001/api/movies`);
+  const { results } = await response.json();
+
+  return {
+    props: {
+      results,
+    },
+  };
 }
